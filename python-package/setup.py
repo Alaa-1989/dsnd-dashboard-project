0d7c307fac3c8498ac7b168fa141d8cf9ae6d817
Alaa Alaboud <alaa.a.alaboud@hotmail.com>
2026-07-20 15:19:36 +0300

    Align python package structure with rubric

--- python-package/setup.py
+++ python-package/setup.py
@@ -2,14 +2,14 @@ from pathlib import Path
 from setuptools import setup, find_packages
 
 cwd = Path(__file__).resolve().parent
-requirements = (cwd / 'employee_events' / 'requirements.txt').read_text().split('\n')  #noqa: E501
+requirements = (cwd / 'requirements.txt').read_text().split('\n')  #noqa: E501
 
 setup_args = dict(
     name='employee_events',
     version='0.0',
     description='SQL Query API',
     packages=find_packages(),
-    package_data={'': ['employee_events.db', 'requirements.txt']},
+    package_data={'': ['employee_events.db']},
     install_requires=requirements,
     )
 
